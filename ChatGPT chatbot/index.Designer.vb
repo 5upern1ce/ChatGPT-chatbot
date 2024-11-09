cf9a3115987bf3127c73759fa703508e1187ad55
Ice <at05266340@priestley.ac.uk>
2024-11-09 11:47:50 +0000

    #

--- ChatGPT chatbot/index.Designer.vb
+++ ChatGPT chatbot/index.Designer.vb
@@ -22,16 +22,29 @@ Partial Class index
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.submitBtn = New System.Windows.Forms.Button()
         Me.SuspendLayout()
+        '
+        'submitBtn
+        '
+        Me.submitBtn.Location = New System.Drawing.Point(308, 326)
+        Me.submitBtn.Name = "submitBtn"
+        Me.submitBtn.Size = New System.Drawing.Size(159, 77)
+        Me.submitBtn.TabIndex = 0
+        Me.submitBtn.Text = "Submit"
+        Me.submitBtn.UseVisualStyleBackColor = True
         '
         'index
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.submitBtn)
         Me.Name = "index"
         Me.Text = "index"
         Me.ResumeLayout(False)
 
     End Sub
+
+    Friend WithEvents submitBtn As Button
 End Class
--- ChatGPT chatbot/index.vb
+++ ChatGPT chatbot/index.vb
@@ -2,4 +2,8 @@
     Private Sub index_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
     End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles submitBtn.Click
+
+    End Sub
 End Class
