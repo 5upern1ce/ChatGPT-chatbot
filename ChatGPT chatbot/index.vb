7f0a3ac7a09985c79c94622cf5ddc3ffbbcad8b7
Ice <at05266340@priestley.ac.uk>
2024-11-09 12:01:13 +0000

    #

--- ChatGPT chatbot/index.vb
+++ ChatGPT chatbot/index.vb
@@ -4,13 +4,21 @@
     End Sub
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles submitBtn.Click
+        Dim yrTxt.Text As String = yrgrp
+            
+        Dim subjectTxt.Text As String = subjct
 
         Dim newForm As New Form1()
         newForm.Show()
-        Hide()
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles yrLbl.Click
+
     End Sub
 
-    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles yrLbl.Click
+    Private Sub yrTxt_TextChanged(sender As Object, e As EventArgs) Handles yrTxt.TextChanged
+
+    End Sub
+
+    Private Sub subjectTxt_TextChanged(sender As Object, e As EventArgs) Handles subjectTxt.TextChanged
 
     End Sub
 End Class
